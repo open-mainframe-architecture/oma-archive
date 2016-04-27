13fc1ff8f4e0b4fff2081afc4409d3d3f33cd085
highdutch <dick.heijink@gmail.com>
2016-04-27 23:35:50 +0200

    es6

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 "use strict";
 
-var path = require('path');
+const path = require('path');
 
-var constants = require('oma-constants');
-var jshint = require('jshint').JSHINT;
-var util = require('oma-util');
+const constants = require('oma-constants');
+const jshint = require('jshint').JSHINT;
+const util = require('oma-util');
 
-var assetPath = {
+const assetPath = {
   bootScript: util.filesPattern(constants.category, constants.module.bootScript),
   bundleScripts: util.filesPattern(constants.category, constants.archive.bundleScripts),
   classScripts: util.filesPattern(constants.category, constants.module.classScripts),
@@ -16,128 +16,108 @@ var assetPath = {
 };
 
 // promise to fill archive by scanning directories for modules
-module.exports = function (error, directories, output) {
-  var archive = util.zip(output);
+module.exports = (error, directories, output) => {
+  const archive = util.zip(output);
   return Promise.all([
     // collect configure scripts
     collectConfigs(error, assetPath.bundleScripts, directories, archive),
     // collect source assets of modules
     collectModules(error, constants.archive.topConfig, directories, archive)
   ])
-    .then(function (results) {
-      return new Promise(function (resolve) {
-        // resolve when archive can be reopened
-        output.on('finish', function() { resolve(results[2]); });
-        // close archive
-        archive.end();
-      });
-    })
+    .then(results => new Promise(function(resolve) {
+      // resolve when archive can be reopened
+      output.on('finish', () => { resolve(results[2]); });
+      // close archive
+      archive.end();
+    }))
     ;
 };
 
 function collectConfigs(error, files, directories, archive) {
-  var configs = {}, patterns = directories.map(function (dir) { return dir + '/' + files; });
-  return util.mapFiles(patterns, function (file, cb) {
-    var dir = path.dirname(file.path);
-    var relative = path.relative(path.dirname(dir), file.path).replace(util.seps, '/');
+  const configs = {}, patterns = directories.map(dir => `${dir}/${files}`);
+  return util.eachFile(patterns, file => {
+    const dir = path.dirname(file.path);
+    const relative = path.relative(path.dirname(dir), file.path).replace(util.seps, '/');
     if (configs[relative]) {
-      throw new Error('Duplicate configuration in archive:' + relative);
+      throw new Error(`Duplicate configuration in archive: ${relative}`);
     }
     configs[relative] = file;
     util.zipFile(archive, relative, file);
-    cb(null);
   })
-    .then(function () {
-      return Promise.all(Object.keys(configs).map(function (relative) {
-        return verifyExpression(error, configs[relative]);
-      }));
-    })
-    .then(function () { })
+    .then(() => Promise.all(Object.keys(configs).map(relative =>
+      verifyExpression(error, configs[relative])
+    )))
+    .then(util.doNothing)
     ;
 }
 
 function collectModules(error, files, directories, archive) {
   return collectTopModules(files, directories, archive)
-    .then(function (modules) {
-      return Promise.all(Object.keys(modules).map(function (name) {
-        // recursively collect configure scripts of submodules
-        return collectSubModules(constants.archive.subConfig, archive, modules, name);
-      }))
-        .then(function () {
-          // configure scripts of all modules are now archived, continue with other assets
-          var names = Object.keys(modules);
-          return Promise.all(names.map(function (name) {
-            var home = modules[name];
-            return Promise.all([
-              verifyExpressions(error, assetPath.bootScript, home),
-              verifyExpressions(error, assetPath.configScript, home),
-              verifyExpressions(error, assetPath.configScripts, home),
-              verifyExpressions(error, assetPath.classScripts, home),
-              archiveFiles(assetPath.bootScript, archive, name, home),
-              archiveFiles(assetPath.configScripts, archive, name, home),
-              archiveFiles(assetPath.classScripts, archive, name, home),
-              archiveFiles(assetPath.publicAssets, archive, name, home)
-            ]);
-          }))
-            .then(function () { return names.sort(); })
-            ;
-        });
-    })
+    .then(modules => Promise.all(Object.keys(modules).map(name =>
+      // recursively collect configure scripts of submodules
+      collectSubModules(constants.archive.subConfig, archive, modules, name)))
+      // configure scripts of all modules are now archived, continue with other assets
+      .then(() => Promise.all(Object.keys(modules).map(name => Promise.all([
+        verifyExpressions(error, assetPath.bootScript, modules[name]),
+        archiveFiles(assetPath.bootScript, archive, name, modules[name]),
+        verifyExpressions(error, assetPath.configScript, modules[name]),
+        verifyExpressions(error, assetPath.configScripts, modules[name]),
+        archiveFiles(assetPath.configScripts, archive, name, modules[name]),
+        verifyExpressions(error, assetPath.classScripts, modules[name]),
+        archiveFiles(assetPath.classScripts, archive, name, modules[name]),
+        archiveFiles(assetPath.publicAssets, archive, name, modules[name])
+      ]))))
+      .then(() => Object.keys(modules).sort()))
+    ;
 }
 
 function collectTopModules(files, directories, archive) {
-  var modules = {};
-  var patterns = directories.map(function (dir) { return dir + '/' + files; });
-  return util.mapFiles(patterns, function (file, cb) {
-    var dir = path.dirname(file.path), name = path.basename(dir);
+  const modules = {};
+  return util.eachFile(directories.map(dir => `${dir}/${files}`), file => {
+    const dir = path.dirname(file.path), name = path.basename(dir);
     if (modules[name]) {
-      throw new Error('Duplicate module in archive: ' + name);
+      throw new Error(`Duplicate module in archive: ${name}`);
     }
     modules[name] = dir;
-    var relative = path.relative(path.dirname(dir), file.path).replace(util.seps, '/');
+    const relative = path.relative(path.dirname(dir), file.path).replace(util.seps, '/');
     util.zipFile(archive, relative, file);
-    cb(null);
   })
-    .then(function () { return modules; })
+    .then(() => modules)
     ;
 }
 
 function collectSubModules(files, archive, modules, name) {
-  return util.mapFiles(modules[name] + '/' + files, function (file, cb) {
+  return util.eachFile(`${modules[name]}/${files}`, file => {
     var dir = path.dirname(file.path), subName = name + '.' + path.basename(dir);
     if (modules[subName]) {
-      throw new Error('Duplicate module in archive: ' + subName);
+      throw new Error(`Duplicate module in archive: ${subName}`);
     }
     modules[subName] = dir;
-    var relative = subName + '/' + path.relative(dir, file.path).replace(util.seps, '/');
+    const relative = `${subName}/${path.relative(dir, file.path).replace(util.seps, '/')}`;
     util.zipFile(archive, relative, file);
-    collectSubModules(files, archive, modules, subName).then(function () { cb(null); });
+    return collectSubModules(files, archive, modules, subName);
   });
 }
 
 function verifyExpression(error, file) {
-  return error && util.readFileText(file)
-    .then(function (expressionSource) {
-      jshint('void\n' + expressionSource + '\n;', constants.tool.jshint);
-      jshint.errors.slice().forEach(function (jsError) {
-        // adjust for extra first line
-        error(file.path + ':' + (jsError.line - 1) + ':' + jsError.character, jsError.reason);
-      });
-    });
+  return Promise.resolve(error && util.readFileText(file).then(expressionSource => {
+    jshint(`void\n${expressionSource}\n;`, constants.tool.jshint);
+    for (let jsError of jshint.errors.slice()) {
+      // adjust for extra first line
+      error(`${file.path}:${jsError.line - 1}:${jsError.character}`, jsError.reason);
+    }
+  }));
 }
 
 function verifyExpressions(error, files, home) {
-  return error && util.mapFiles(home + '/' + files, function (file, cb) {
-    verifyExpression(error, file)
-      .then(function () { cb(null); })
-    ;
-  });
+  return Promise.resolve(
+    error && util.eachFile(`${home}/${files}`, file => verifyExpression(error, file))
+  );
 }
 
 function archiveFiles(files, archive, name, home) {
-  return util.mapFiles(home + '/' + files, function (file, cb) {
-    var relative = name + '/' + path.relative(home, file.path).replace(util.seps, '/');
+  return util.eachFile(`${home}/${files}`, file => {
+    const relative = `${name}/${path.relative(home, file.path).replace(util.seps, '/')}`;
     util.zipFile(archive, relative, file);
-    cb(null);
   });
 }
